a1c5d2f6b7df178397f1803e7db7d30ca187bfec
eliachi2003 <eliachi2003@gmail.com>
2026-01-20 18:40:26 -0400

    Revert "Proyecto SignalSpeak"
    This reverts commit 5a73c17b380f1c7f68e124ab925005f95e74e342, reversing
    changes made to e57f7516f1e32be999828862adcc4932a067a761.

--- SignalSpeak/SignalSpeak/Program.cs
+++ SignalSpeak/SignalSpeak/Program.cs
@@ -1,57 +1,27 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using SignalSpeak.Components;
-using SignalSpeak.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services
-    .AddDefaultIdentity<IdentityUser>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = false;
-    })
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddAuthorization();
-
-// Register DbContext
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddAuthorization();
-
-// Blazor services
+// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddHttpClient();
-
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-// ✅ Add Anti-Forgery middleware here
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-app.UseAntiforgery();
 
 app.Run();
