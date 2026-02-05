75034ac78cce59e8851f44f4b9b7508a14fb7eef
RicardoAbrams <rickyabrams2018@gmail.com>
2026-02-04 20:53:31 -0400

    Changed SignalSpeak title to matching logo SignSpeak, Added profile section in the left navbar of the main menu and added a new LogoutConfirmation.razor that includes a button that directs to be logged out.

--- SignalSpeak/SignalSpeak/obj/Debug/net8.0/SignalSpeak.AssemblyInfo.cs
+++ SignalSpeak/SignalSpeak/obj/Debug/net8.0/SignalSpeak.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalSpeak")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a33b3bf437034b447ef6b76862e9137f13ccaec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1106612b2448f9ac536f1feaae6ff3c71e03a3ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalSpeak")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalSpeak")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
